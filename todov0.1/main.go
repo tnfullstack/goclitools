fc3b82220956e2a7f91947a6938dbe58927937cf
Thanh Nguyen <chrisng9@outlook.com>
2022-02-03 17:36:42 -0800

    adding main_test.go

--- todov0.1/main.go
+++ todov0.1/main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"clitools/todov0.1/todo"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
-	l := todo.List{}
-	taskName := "New Task"
+// Hardcoding the file name
+const todoFilename = "data/todo.json"
 
-	// Add 5 task to the list
-	for i := 0; i <= 5; i++ {
-		str := fmt.Sprintf("%s %d", taskName, i)
-		l.Add(str)
-		fmt.Println(l[i].Task)
+func main() {
+	// Define a items list
+	l := &todo.List{}
+
+	// Get reads the item from the file
+	err := l.Get(todoFilename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on the number of arguments prvovided
+	switch {
+	// Print the list when no arguments
+	case len(os.Args) == 1:
+		// List current todo items
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	// Concatenate all prvovided arguments with a space and add to
+	// the list as an item
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		// add the task
+		l.Add(item)
+	}
+
+	// Save the new list
+	if err := l.Save(todoFilename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
